0131ef3e59da6b3025a08dad03d314cf2eebcc12
Araame <arame171002@gmail.com>
2026-05-14 14:41:30 +0000

    Models done

--- sta/admin.py
+++ sta/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
+from .models import Note, Category, CustomUser, department
 # Register your models here.
+
+admin.site.register(Note)
+admin.site.register(Category)
+admin.site.register(CustomUser)
+admin.site.register(department)
+
+
+
--- sta/models.py
+++ sta/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class department(models.Model):
+    title = models.CharField(max_length=25)
+
+class CustomUser(AbstractUser):
+    department = models.ForeignKey(department, on_delete=models.SET_NULL,max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+class Category(models.Model):
+    category_title = models.CharField(max_length=20, verbose_name="Category's title")
+    category_description = models.TextField(verbose_name="Category's description")
+
+    def __str__(self):
+        return self.category_title
+
+class Note (models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField(upload_to= "media/images")
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+
+    
